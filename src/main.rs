596b3663eae7137f55da8f62206faaf6f8d1df7d
J0L0HD <juliantepper2006gmail.com>
2025-01-01 20:23:06 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use tracing;
+use tracing_subscriber;
+
+fn main() {
+    let file_appender = tracing_appender::rolling::minutely("./logs", "debug.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt().with_writer(non_blocking).init();
+    tracing::info!("Hello, world!");
+}
