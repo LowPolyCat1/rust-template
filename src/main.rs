3e4d2a1286791f0643b552707d20fc681f12587f
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-02-08 23:06:10 +0100

    removed tracing appender

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-    let file_appender = tracing_appender::rolling::minutely("./logs", "debug.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    tracing_subscriber::fmt().with_writer(non_blocking).init();
+    tracing_subscriber::fmt().init();
     tracing::info!("Hello, world!");
 }
